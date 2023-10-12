ab7bfcde6bc78b22149f8f47249f684018202e34
sanghak <yee950419@naver.com>
2023-10-12 11:20:16 +0900

    first commit

---
+++ src/main/java/com/study/jwtPractice/JwtPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.study.jwtPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtPracticeApplication.class, args);
+	}
+
+}
